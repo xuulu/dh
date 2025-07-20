a341e598cb5f9bb4c0fa751418ec16eb8998c5f5
Administrator <xuulu04@gmail.com>
2025-07-20 17:48:47 +0800

    第一次提交代码

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+
+    output: "export", // 静态导出
+    reactStrictMode: true,  // 开启严格模式 用于开发阶段的错误检测，生命周期和副作用会运行两次
+
 };
 
 export default nextConfig;
